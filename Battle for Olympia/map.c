aa6c0cafbaf5711665e36f0a1a185c68e1f94d0b
dickyadrian <dickyadrian0204@gmail.com>
2017-11-23 17:50:14 +0700

    ADT map commented

--- Battle for Olympia/map.c
+++ Battle for Olympia/map.c
@@ -25,6 +25,8 @@ const Grid EMPTYGRID = {
 Map map;
 
 void CreateMap(int N, int M)
+/* I.S. : Sembarang */
+/* F.S. : Membuat Map dengan ukuran NxM yang bertipe matriks */
 {
 	int i;
 	MakeMATRIKS(&map,N,M);
@@ -39,42 +41,64 @@ void CreateMap(int N, int M)
 	}
 }
 
-POINT GetCoordinate(Grid grid) {
+POINT GetCoordinate(Grid grid) 
+/* Mengembalikan koordinat petak dalam tipe bentukan point */
+{
     return grid.coordinate;
 }
 
-Type GetType(Grid grid) {
+Type GetType(Grid grid) 
+/* Mengembalikan type suatu petak */
+{
     return grid.type;
 }
 
-Player *GetOwner(Grid grid) {
+Player *GetOwner(Grid grid)
+/* Mengembalikan alamat player yang memiliki petak */
+{
     return grid.owner;
 }
 
-void SetOwner(Grid *grid, Player *owner) {
+void SetOwner(Grid *grid, Player *owner) 
+/* I.S. : Sembarang petak terdefinisi*/
+/* F.S. : Petak yang ditunjuk menjadi pemilik owner */
+{
     grid->owner = owner;
 }
 
-Unit *GetUnit(Grid grid) {
+Unit *GetUnit(Grid grid) 
+/* Mengembalikan unit yang ada di petak tersebut */
+{
     return grid.unit;
 }
 
-void SetUnit(Grid *grid, Unit *unit) {
+void SetUnit(Grid *grid, Unit *unit) 
+/* I.S. : Sembarang, petak terdefinisi */
+/* F.S. : petak tersebut ditempati oleh unit tertentu */
+{
     grid->unit = unit;
 }
 
-char GetGridColor(Grid grid) {
+char GetGridColor(Grid grid) 
+/* Mengembalikan warna dari pemmilik petak */
+{
     return GetColor(*grid.owner);
 }
 
-Grid *GetGrid(int x, int y) {
+Grid *GetGrid(int x, int y) 
+/* Mengembalikan petak dari absis x dan ordinat y */
+{
     return &(map.Mem[GetMapSizeM() * y + x]);
 }
 
-int GetMapSizeN() {
+int GetMapSizeN() 
+/* mengembalikan N dari matriks NxM */
+{
     return map.N;
 }
 
-int GetMapSizeM() {
+int GetMapSizeM() 
+/* mengembalikan M dari matriks NxM*/
+{
     return map.M;
 }
--- Battle for Olympia/map.h
+++ Battle for Olympia/map.h
@@ -1,3 +1,7 @@
+/*File : map.h*/
+/* Deklarasi type yang digunakan pada map */
+/* Deklarasi fungsi-fungsi yang digunakan untuk mengakses map */
+
 #ifndef MAP_H
 #define MAP_H
 
@@ -14,25 +18,39 @@ extern Map map;
 const Grid EMPTYGRID;
 
 void CreateMap(int N, int M);
+/* I.S. : Sembarang */
+/* F.S. : Membuat Map dengan ukuran NxM yang bertipe matriks */
 
 POINT GetCoordinate(Grid grid);
+/* Mengembalikan koordinat petak dalam tipe bentukan point */
 
 Type GetType(Grid grid);
+/* Mengembalikan type suatu petak */
 
 Player *GetOwner(Grid grid);
+/* Mengembalikan alamat player yang memiliki petak */
 
 void SetOwner(Grid *grid, Player *owner);
+/* I.S. : Sembarang petak terdefinisi*/
+/* F.S. : Petak yang ditunjuk menjadi pemilik owner */
 
 Unit *GetUnit(Grid grid);
+/* Mengembalikan unit yang ada di petak tersebut */
 
 void SetUnit(Grid *grid, Unit *unit);
+/* I.S. : Sembarang, petak terdefinisi */
+/* F.S. : petak tersebut ditempati oleh unit tertentu */
 
 char GetGridColor(Grid grid);
+/* Mengembalikan warna dari pemmilik petak */
 
 Grid *GetGrid(int x, int y);
+/* Mengembalikan petak dari absis x dan ordinat y */
 
 int GetMapSizeN();
+/* mengembalikan N dari matriks NxM */
 
 int GetMapSizeM();
+/* mengembalikan M dari matriks NxM*/
 
 #endif
