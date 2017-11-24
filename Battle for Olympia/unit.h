e980d7e10c3cf65e5615eeb0f0efcf3c1c96b2dc
FerdiantJoshua <ferdiant.98@gmail.com>
2017-11-24 10:14:36 +0700

    add the cursed Movement = '#'

--- Battle for Olympia/unit.h
+++ Battle for Olympia/unit.h
@@ -9,7 +9,7 @@
 
 #include <stdlib.h>
 
-typedef enum EUnitClass {King = 'K', Archer = 'A', Swordsman = 'S', WhiteMage = 'W'} UnitClass;
+typedef enum EUnitClass {King = 'K', Archer = 'A', Swordsman = 'S', WhiteMage = 'W', Movement = '#'} UnitClass;
 typedef enum EAttackType {Melee, Ranged} AttackType;
 
 typedef struct SUnit {
