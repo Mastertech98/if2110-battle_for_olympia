4ebe778b5304574e324380347f06a0bdb1c99ebb
FerdiantJoshua <ferdiant.98@gmail.com>
2017-11-25 23:25:52 +0700

    perfect move

--- Battle for Olympia/Commands/move.h
+++ Battle for Olympia/Commands/move.h
@@ -1,33 +1,22 @@
 #ifndef MOVE_H
 #define MOVE_H
 
-#include "../corefiles.h"
+//#include "../corefiles.h"
+#include "attack.h"
+#include "recruit.h"
+#include "move.h"
 #include "../ADT/stackt.h"
 
 #define DUMVAL 999
 
 extern Stack S_moves;
 
-const Unit UNITMOV = {
-    .unitClass = Movement,
-    .color = 'G',
-
-    .maximumHealth = DUMVAL,
-    .health = DUMVAL,
-    .attack = DUMVAL,
-    .maximumMovementPoints = DUMVAL,
-    .movementPoints = DUMVAL,
-    .attackType = Melee,
-    .chanceAttack = true,
-    .location.X = -999,
-    .location.Y = -999,
-    .price = DUMVAL
-};
+const Unit UNITMOV;
 
 void Move(Unit *unit, Map *map);
 void MakeMovementMap(Unit unit);
 void ClearMovementMap(Map *map);
 boolean IsValidUndo();
-void Undo(Unit *U);
+void Undo();
 
 #endif // MOVE_H
